2ddf907cefcc191fb37270bb2dd485d337a1608b
veroniccc <shakarishviliveronica09@gmail.com>
2024-01-18 14:44:06 +0400

    ...

---
+++ script.js
@@ -0,0 +1,23 @@
+console.log("1 homework");
+console.log("my name is veronica");
+console.log("44");
+
+var name = "veronica";
+console.log(name);
+var username = 'shakarishvili';
+console.log(username);
+
+let name = 'veronica';
+let username = 'kobaladze';
+let age = 24;
+console.log(name + " has a friend "  + usernamesurname +  " and they are  "  + age + "years old");
+console.log("$(name) has a friend $(username)  and they are $(age) years old     ")
+
+
+let x = 7;
+let y = 5;
+let c = 2;
+let z = x + y * c;
+console.log(z);
+
+
